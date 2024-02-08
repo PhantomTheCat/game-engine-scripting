100c88b712ac4558849fcc303a3abbff6542cfca
PhantomTheCatto <aidenmax@comcast.net>
2024-02-08 11:54:04 -0600

    Finished my Battleship Game! - Added in images for the icons, and turned them into 2D UI Sprites for usage - Added in a reset button that would restart the app and set all icons back to original state - Added randomization to ship's spawn locations - Added in the timer that would keep track of time - Coded in all the desired specifications

--- Assets/Week-3/Scripts/GameManager.cs
+++ Assets/Week-3/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 
 namespace Battleship
@@ -44,6 +46,10 @@ namespace Battleship
         [SerializeField] TextMeshProUGUI timeLabel;
         [SerializeField] TextMeshProUGUI scoreLabel;
 
+        //Will keep track of if the board is set up
+        //Doesn't allow game to create another board while one is up
+        bool isBoardSetUp = false;
+
 
         //Methods
         private void Awake()
@@ -52,16 +58,43 @@ namespace Battleship
             numberOfRows = grid.GetLength(0);
             numberOfCols = grid.GetLength(1);
 
+
+            //Setting up the grid with new values that are random (Part of the assignment)
+            //Selecting every row
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                //Selecting every column
+                for (int col = 0; col < numberOfCols; col++)
+                {
+                    //Produces either 0 or 1
+                    int randomNumber = Random.Range(0, 2);
+
+                    //Setting the instance here in this spot to be 0 or 1
+                    grid[row, col] = randomNumber;
+
+                }
+            }
+
             //Creating an identical 2D Array of our grid of the type bool rather than int
             hits = new bool[numberOfRows, numberOfCols];
 
-            //Populating the grid (Figuring out how much to execute by multiplying rows by columns)
-            for (int i = 0; i < numberOfRows * numberOfCols; i++)
+            //Checking if we already have board up
+            if (isBoardSetUp == false)
             {
-                Instantiate(cellPrefab, gridRoot);
+                //Populating the grid (Figuring out how much to execute by multiplying rows by columns)
+                for (int i = 0; i < numberOfRows * numberOfCols; i++)
+                {
+                    Instantiate(cellPrefab, gridRoot);
+                }
+
+                //Setting this variable to let game know we have the board
+                isBoardSetUp = true;
             }
 
             SelectCurrentCell();
+
+            //Repeating the incrementing of time every second
+            InvokeRepeating("IncrementTime", 1f, 1f);
         }
 
         Transform GetCurrentCell()
@@ -134,7 +167,7 @@ namespace Battleship
         {
             //Gets current cell
             Transform cell = GetCurrentCell();
-            //Setting the hit image to active
+            //Setting the miss image to active
             Transform miss = cell.Find("Miss");
             miss.gameObject.SetActive(true);
         }
@@ -163,11 +196,115 @@ namespace Battleship
             {
                 ShowHit();
                 IncrementScore();
+
+                //Trying to end the game if we hit something
+                TryEndGame();
             }
             else
             {
                 ShowMiss();
             }
+        }
+
+        void TryEndGame()
+        {
+            //Checking every row
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                //Checking every column
+                for (int col = 0; col < numberOfCols; col++)
+                {
+                    //Ignoring spots on our grid that aren't ships
+                    if (grid[row, col] == 0)
+                    {
+                        continue;
+                    }
+                    //Stopping the search and method if we find at least one ship that hasn't been hit
+                    if (hits[row, col] == false) 
+                    {
+                        return;
+                    }
+                }
+
+            }
+
+            //Putting the winLabel on as if we get to this, it means that all ships have been hit
+            winLabel.SetActive(true);
+
+            //Stoping the timer and the InvokeRepeating method we called above
+            CancelInvoke("IncrementTime");
+        }
+
+        void IncrementTime()
+        {
+            //Incrementing the time each second
+            time++;
+
+            //Updating the time label
+            //The {0} stands for the minutes of the game, which is shown by time / 60
+            //The {1} stands for the seconds of the game up to 60 (Always 2 digits)
+            timeLabel.text = string.Format("{0}:{1}", time/60, (time % 60).ToString("00"));
+        }
+
+        public void RestartGame()
+        {
+            //Restarting the game by setting everything to defaults
+            UnselectCurrentCell();
+            currentSelectedCol = 0;
+            currentSelectedRow = 0;
+            SelectCurrentCell();
+
+            //Resetting the 2D hits array to be blank
+            //And Removing the hit and miss icons from the board (Set them to inactive)
+            //Selecting every row
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                //Selecting every column
+                for (int col = 0; col < numberOfCols; col++)
+                {
+                    //Setting the logic up for GetCurrentCell
+                    currentSelectedCol = col;
+                    currentSelectedRow = row;
+
+                    //Sets the instance of that hit to be false
+                    hits[row, col] = false;
+
+                    //Gets current cell
+                    Transform cell = GetCurrentCell();
+
+                    //Setting the hit image to not active
+                    Transform hit = cell.Find("Hit");
+                    hit.gameObject.SetActive(false);
+
+                    //Setting the miss image to not active
+                    Transform miss = cell.Find("Miss");
+                    miss.gameObject.SetActive(false);
+                }
+            }
+
+            //Reseting these values are searching through both grids
+            UnselectCurrentCell();
+            currentSelectedCol = 0;
+            currentSelectedRow = 0;
+            SelectCurrentCell();
+
+            //Resetting timer and score, and their labels
+            time = 0;
+            timeLabel.text = string.Format("{0}:{1}", time / 60, (time % 60).ToString("00"));
+            score = 0;
+            scoreLabel.text = string.Format("Score: {0}", score);
+
+
+
+            //Setting the winLabel to be inactive
+            winLabel.SetActive(false);
+
+
+
+            //For the assignment, the randomization of the locations of the ships is included in my awake function
+            //This will allow it to be called upon to randomize at the start and every time the game is reset.
+            //Reseting up the game
+            Awake();
         }
     }
 
