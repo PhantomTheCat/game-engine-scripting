adf370eefcb86e3eb03883b0dc4b672209333d3a
PhantomTheCatto <aidenmax@comcast.net>
2024-02-12 20:18:28 -0600

    Worked on the Hanoi Tower in class for Week #4! - It's still located in week #3 - Made the template for week #4, with folders for scripts, scenes, etc.

--- Assets/Week-3/Scripts/HanoiTower.cs
+++ Assets/Week-3/Scripts/HanoiTower.cs
@@ -5,130 +5,196 @@ using UnityEngine;
 public class HanoiTower : MonoBehaviour
 {
     //Properties
-    int[] peg1 = { 1, 2, 3, 4, 5, 6, 7, 8 };
-    int[] peg2 = { 0, 0, 0, 0, 0, 0, 0, 0 };
-    int[] peg3 = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
-    int currentPeg = 1;
+    [SerializeField] private Transform peg1Transform;
+    [SerializeField] private Transform peg2Transform;
+    [SerializeField] private Transform peg3Transform;
+
+
+    [SerializeField] private int[] peg1 = { 1, 2, 3, 4 };
+    [SerializeField] private int[] peg2 = { 0, 0, 0, 0 };
+    [SerializeField] private int[] peg3 = { 0, 0, 0, 0 };
+
+    [SerializeField] private int currentPeg = 1;
 
 
     //Methods
-    int[] GetPeg(int peg)
-    {
-        if (peg == 1) 
-        {
-            return peg1;
-        }
-        else if (peg == 2)
-        {
-            return peg2;
-        }
-        else if (peg == 3)
-        {
-            return peg3;
-        }
-        else 
-        {
-            return null;
-        }
-    }
-
     [ContextMenu("Move Right")]
-    void MoveRight()
+    public void MoveRight()
     {
-        //Get Lists we are working with
-        int[] currentList = GetPeg(currentPeg);
-        int[] targetList = GetPeg(currentPeg + 1);
+        //Make sure we aren't the right most peg
+        if (CanMoveRight() == false) return;
 
-        //Stops procedure if we didn't get a next target list, which stops it from going to a 4th array
-        if (targetList == null) 
-        {
-            return;
-        }
+        //Check to see what index and number we are moving from THIS peg
+        int[] fromArray = GetPeg(currentPeg);
+        int fromIndex = GetTopNumberIndex(fromArray);
 
-        //Get Top number index from the current list
-        int fromIndex = GetTopNumberIndex(currentList);
-        //Get the bottom most free index from the target list
-        int toIndex = GetBottomNumberIndex(targetList);
+        //If there wasn't anything to move then don't try to move
+        if (fromIndex == -1) return;
 
-        //Check that we are able to find a free spot in the target list
-        if(toIndex == -1)
-        {
-            return;
-        }
+        //Check to see where in the peg we are moving to that the number
+        //should be placed into
+        int[] toArray = GetPeg(currentPeg + 1);
+        int toIndex = GetIndexOfFreeSlot(toArray);
 
-        //Check that the number we want to move does not break our rules
-        if (CanMoveIntoPeg(currentList[fromIndex], currentList) == false)
-        {
-            return;
+        //If the adjacent peg is FULL then we cannot move anything into it
+        //This probably will never happen since the max number of numbers
+        //we have is the size of each peg
+        if (toIndex == -1) return;
 
-        }
+        //Lastly check to verify the number we are moving is not larger
+        //than whatever number we may be placing this number on top of
+        //on the adjacent peg
+        if (CanAddToPeg(fromArray[fromIndex], toArray) == false) return;
 
-        MoveIntoPeg(fromIndex, toIndex, currentList, targetList);
+        //If all checks PASS then go aheand and move the number
+        //out of THIS array into the adjacent array
+        MoveNumber(fromArray, fromIndex, toArray, toIndex);
 
+        //Getting our disc to move and peg to move it to
+        Transform disc = PopDiscFromCurrentPeg();
+        Transform toPeg = GetPegTransform(currentPeg + 1);
+
+        //Setting the disc to it's new peg parent (Will move it in interface)
+        disc.SetParent(toPeg);
     }
 
 
     [ContextMenu("Move Left")]
-    void MoveLeft() 
+    public void MoveLeft()
     {
-        
-    
+        //Make sure we aren't the left most peg
+        if (CanMoveLeft() == false) return;
+
+        //Check to see what index and number we are moving from THIS peg
+        int[] fromArray = GetPeg(currentPeg);
+        int fromIndex = GetTopNumberIndex(fromArray);
+
+        //If there wasn't anything to move then don't try to move
+        if (fromIndex == -1) return;
+
+        //Check to see where in the peg we are moving to that the number
+        //should be placed into
+        int[] toArray = GetPeg(currentPeg - 1);
+        int toIndex = GetIndexOfFreeSlot(toArray);
+
+        //If the adjacent peg is FULL then we cannot move anything into it
+        //This probably will never happen since the max number of numbers
+        //we have is the size of each peg
+        if (toIndex == -1) return;
+
+        //Lastly check to verify the number we are moving is not larger
+        //than whatever number we may be placing this number on top of
+        //on the adjacent peg
+        if (CanAddToPeg(fromArray[fromIndex], toArray) == false) return;
+
+        //If all checks PASS then go aheand and move the number
+        //out of THIS array into the adjacent array
+        MoveNumber(fromArray, fromIndex, toArray, toIndex);
+
+
+        //Getting our disc to move and peg to move it to
+        Transform disc = PopDiscFromCurrentPeg();
+        Transform toPeg = GetPegTransform(currentPeg - 1);
+
+        //Setting the disc to it's new peg parent (Will move it in interface)
+        disc.SetParent(toPeg);
     }
 
-    int GetTopNumberIndex(int[] peg) 
+
+    public void IncrementPegNumber()
     {
-        for(int i = 0; i < peg.Length; i++)
+        if (currentPeg != 3)
         {
-            //if value in peg for index is NOT 0
-            if (peg[i] != 0) 
-            {
-                return i;
-            }
+            currentPeg++;
+        }
+    }
+
+    public void DecrementPegNumber()
+    {
+        if (currentPeg != 1)
+        {
+            currentPeg--;
+        }
+    }
+
+
+    Transform PopDiscFromCurrentPeg()
+    {
+        //Getting our selected peg from the Unity interface
+        Transform currentPegTransform = GetPegTransform(currentPeg);
+        int index = currentPegTransform.childCount - 1;
+
+        //Getting the child of that peg (The disk that we will be moving)
+        Transform disk = currentPegTransform.GetChild(index);
+        return disk;
+    }
+
+    Transform GetPegTransform(int pegNumber)
+    {
+        if (pegNumber == 1) return peg1Transform;
+
+        if (pegNumber == 2) return peg2Transform;
+
+        return peg3Transform;
+    }
+
+
+    void MoveNumber(int[] fromArr, int fromIndex, int[] toArr, int toIndex)
+    {
+        int value = fromArr[fromIndex];
+        fromArr[fromIndex] = 0;
+
+        toArr[toIndex] = value;
+    }
+
+    bool CanMoveRight()
+    {
+        //If peg 1 or 2 then can move right
+        return currentPeg < 3;
+    }
+
+    bool CanAddToPeg(int value, int[] peg)
+    {
+        int topNumberIndex = GetTopNumberIndex(peg);
+        if (topNumberIndex == -1) return true;
+
+        int topNumber = peg[topNumberIndex];
+        return topNumber > value;
+    }
+
+    bool CanMoveLeft()
+    {
+        //If peg 2 or 3 then can move right
+        return currentPeg > 1;
+    }
+
+    int[] GetPeg(int pegNumber)
+    {
+        if (pegNumber == 1) return peg1;
+
+        if (pegNumber == 2) return peg2;
+
+        return peg3;
+    }
+
+    int GetTopNumberIndex(int[] peg)
+    {
+        for (int i = 0; i < peg.Length; i++)
+        {
+            if (peg[i] != 0) return i;
         }
 
-
-        //Will tell if they didn't find a number
         return -1;
     }
 
-
-    int GetBottomNumberIndex(int[] peg) 
+    int GetIndexOfFreeSlot(int[] peg)
     {
-        //Same as Getting the top number index, but going backwards
         for (int i = peg.Length - 1; i >= 0; i--)
         {
-            //if value in peg for index is 0
-            if (peg[i] == 0)
-            {
-                return i;
-            }
+            if (peg[i] == 0) return i;
         }
 
-
-        //Will tell if they didn't find a number
         return -1;
     }
-
-    bool CanMoveIntoPeg(int numberToMove, int[] peg) 
-    {
-        int bottomIndex = GetBottomNumberIndex(peg);
-
-        //Checking if 
-        if (bottomIndex == peg.Length - 1 && peg[peg.Length - 1] == 0)
-        {
-            return true;
-        }
-
-        int bottomPlus1 = bottomIndex + 1;
-        return bottomPlus1 == 0;
-    }
-
-    void MoveIntoPeg(int fromIndex, int toIndex, int[] from, int[] to)
-    {
-        int numberToMove = from[fromIndex];
-        from[fromIndex] = 0;
-        to[fromIndex] = numberToMove;
-    }
-
 }
