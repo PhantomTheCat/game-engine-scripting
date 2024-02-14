aec5db86ba4f8236e1e16e9b5daf707c8224b94f
PhantomTheCatto <aidenmax@comcast.net>
2024-02-14 13:56:26 -0600

    Completed my Hanoi Tower Assignment! - Moved it to my Week 4 Folder as I worked more on it this week - Added in comments to the code to explain - Added in logic for displaying a win screen and for updating a text bar that displays, which peg player is on - Added numbers to the pegs for the player

--- Assets/Week-4/Scripts/HanoiTower.cs
+++ Assets/Week-4/Scripts/HanoiTower.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HanoiTower : MonoBehaviour
 {
     //Properties
-
     [SerializeField] private Transform peg1Transform;
     [SerializeField] private Transform peg2Transform;
     [SerializeField] private Transform peg3Transform;
 
+    public TextMeshProUGUI currentPegText;
+    public GameObject winText;
 
     [SerializeField] private int[] peg1 = { 1, 2, 3, 4 };
     [SerializeField] private int[] peg2 = { 0, 0, 0, 0 };
@@ -57,8 +59,11 @@ public class HanoiTower : MonoBehaviour
 
         //Setting the disc to it's new peg parent (Will move it in interface)
         disc.SetParent(toPeg);
-    }
 
+        //Checking if player won and setting winText as visible if they did
+        bool playerWon = CheckIfPlayerWon(peg3);
+        if (playerWon) winText.SetActive(true);
+    }
 
     [ContextMenu("Move Left")]
     public void MoveLeft()
@@ -99,14 +104,18 @@ public class HanoiTower : MonoBehaviour
 
         //Setting the disc to it's new peg parent (Will move it in interface)
         disc.SetParent(toPeg);
-    }
 
+        //Checking if player won and setting winText as visible if they did
+        bool playerWon = CheckIfPlayerWon(peg3);
+        if (playerWon) winText.SetActive(true);
+    }
 
     public void IncrementPegNumber()
     {
         if (currentPeg != 3)
         {
             currentPeg++;
+            UpdatePegText();
         }
     }
 
@@ -115,9 +124,15 @@ public class HanoiTower : MonoBehaviour
         if (currentPeg != 1)
         {
             currentPeg--;
+            UpdatePegText();
         }
     }
 
+    public void UpdatePegText()
+    {
+        //Updating the text in Unity that tells user which peg they are on
+        currentPegText.text = $"Your Current Peg is: {currentPeg}";
+    }
 
     Transform PopDiscFromCurrentPeg()
     {
@@ -132,6 +147,8 @@ public class HanoiTower : MonoBehaviour
 
     Transform GetPegTransform(int pegNumber)
     {
+        //Getting which peg we are transforming our disks too in Unity
+        //Based on our current peg
         if (pegNumber == 1) return peg1Transform;
 
         if (pegNumber == 2) return peg2Transform;
@@ -139,28 +156,30 @@ public class HanoiTower : MonoBehaviour
         return peg3Transform;
     }
 
-
     void MoveNumber(int[] fromArr, int fromIndex, int[] toArr, int toIndex)
     {
+        //Moving the number on top of one array to the top of another array
+        //Logic found out for the values of these passed in variables earlier
         int value = fromArr[fromIndex];
         fromArr[fromIndex] = 0;
 
         toArr[toIndex] = value;
     }
 
-    bool CanMoveRight()
-    {
-        //If peg 1 or 2 then can move right
-        return currentPeg < 3;
-    }
-
     bool CanAddToPeg(int value, int[] peg)
     {
+        //Checking if we can add to a peg
         int topNumberIndex = GetTopNumberIndex(peg);
         if (topNumberIndex == -1) return true;
 
         int topNumber = peg[topNumberIndex];
         return topNumber > value;
+    }
+
+    bool CanMoveRight()
+    {
+        //If peg 1 or 2 then can move right
+        return currentPeg < 3;
     }
 
     bool CanMoveLeft()
@@ -171,6 +190,7 @@ public class HanoiTower : MonoBehaviour
 
     int[] GetPeg(int pegNumber)
     {
+        //Returning the peg we are on
         if (pegNumber == 1) return peg1;
 
         if (pegNumber == 2) return peg2;
@@ -180,21 +200,43 @@ public class HanoiTower : MonoBehaviour
 
     int GetTopNumberIndex(int[] peg)
     {
+        //Getting the top number (earliest number in list with value that isn't 0)
         for (int i = 0; i < peg.Length; i++)
         {
             if (peg[i] != 0) return i;
         }
 
+        //Returning a bad value if there were no numbers that werent 0
         return -1;
     }
 
     int GetIndexOfFreeSlot(int[] peg)
     {
+        //Checking for earliest free slot
         for (int i = peg.Length - 1; i >= 0; i--)
         {
             if (peg[i] == 0) return i;
         }
 
+        //Returns a bad value if there were no free slots
         return -1;
+    }
+
+    bool CheckIfPlayerWon(int[] lastPeg)
+    {
+        //Passes in a peg (the last peg in the series)
+        //Want to check the peg for if it has all the disks in it
+        for (int i = 0; i < lastPeg.Length; i++)
+        {
+            //Checking if any are 0, as each slot should be filled up for a win
+            if (lastPeg[i] == 0)
+            {
+                //If any are, we return false
+                return false;
+            }
+        }
+
+        //Will set it to be true if it made it through the check
+        return true;
     }
 }
